2052ba3bf56a3665cbeb0b295a247d6de25fb976
Ânderson Pinto Alves <andersonpalves@gmail.com>
2026-04-28 12:00:25 -0300

    Atualizando exercicios

---
+++ Lógica_e_Técnicas_de_Programação/python/web/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/resultado', methods=['POST'])
+def resultado():
+    nome = request.form['nome']
+    return render_template('resultado.html', nome=nome)
+
+if __name__ == '__main__':
+    app.run(debug=True)
